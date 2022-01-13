df7cf7ccef4d7212ade83e688845f02e7d23575c
Gtp87 <fabio.giannitrapani@gmail.com>
2022-01-13 15:51:41 +0100

    start header

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
     <Header />
-    <Main />
-    <Footer />
+    <!-- <Main />
+    <Footer /> -->
     <!-- <img alt="Vue logo" src="./assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" /> -->
   </div>
@@ -11,16 +11,16 @@
 <script>
 // import HelloWorld from "./components/HelloWorld.vue";
 import Header from "./components/Header.vue";
-import Main from "./components/Main.vue";
-import Footer from "./components/Footer.vue";
+// import Main from "./components/Main.vue";
+// import Footer from "./components/Footer.vue";
 
 export default {
   name: "App",
   components: {
     // HelloWorld,
     Header,
-    Main,
-    Footer,
+    // Main,
+    // Footer,
   },
 };
 </script>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,13 +1,46 @@
 <template>
-  
+  <header class="container">
+      <div class="header-sx">
+          <img src="../assets/img/dc-logo.png" alt="logo-dc">
+      </div>
+      <div class="header-dx">
+          <ul>
+              <li> <a href="">characters</a></li>
+              <li> <a href="">comics</a></li>
+              <li> <a href="">movies</a></li>
+              <li> <a href="">tv</a></li>
+              <li> <a href="">games</a></li>
+              <li> <a href="">collectibles</a></li>
+              <li> <a href="">videos</a></li>
+              <li> <a href="">fans</a></li>
+              <li> <a href="">news</a></li>
+              <li> <a href="">shop</a></li>
+          </ul>
+      </div>
+  </header>
 </template>
 
 <script>
 export default {
-    name: 'Header'
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+    header {
+    display: flex;
+    
+    }
+    ul {
+        display: flex;
+        li {
+            list-style-type: none;
+            margin: 0 1em;
+            justify-content: end;
+            justify-items: center;
+            a {
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+        }
+    }
 </style>
