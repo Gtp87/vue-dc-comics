82598ac62695b1250f5b68d42f62d2685327fc83
Gtp87 <fabio.giannitrapani@gmail.com>
2022-01-14 16:18:29 +0100

    add product.vue

---
+++ src/components/Product.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="thumb">
+        <div class="img-container">
+            <img :src="img" :alt="alt">
+        </div>
+        <span class="subtitle">{{title}}</span>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Product",
+    props: ["img", "alt", "title", "type", "price"],
+    data(){
+        return {
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.thumb {
+    width: 150px;
+    .img-container {
+        height: 150px;
+        width: 150px;
+        overflow: hidden;
+        margin-bottom: 1em;
+        img {
+            width: 100%;
+        }
+    }
+    .subtitle {
+        color: white;
+        text-transform: uppercase;
+        font-size: 0.8em;
+    }
+}
+</style>
